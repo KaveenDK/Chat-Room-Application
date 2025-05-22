ff6c8b85cd39217705ecc3c913014666ee4335fa
KaveeN <vpdkkaveen@gmail.com>
2025-05-22 20:02:26 +0530

    added ChatServer

--- src/main/java/lk/ijse/edu/chatroomapplication/ChatServer.java
+++ src/main/java/lk/ijse/edu/chatroomapplication/ChatServer.java
@@ -1,5 +1,12 @@
 package lk.ijse.edu.chatroomapplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashSet;
+
 /**
  * --------------------------------------------
  * @Author Dimantha Kaveen
@@ -11,4 +18,73 @@ package lk.ijse.edu.chatroomapplication;
  **/
 
 public class ChatServer {
+
+    private static final int PORT = 8080;
+    private static HashSet<String> names = new HashSet<String>();
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    private static class Handler implements Runnable {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                while (true) {
+                    out.println("SUBMIT NAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    if (!names.contains(names)) {
+                        names.add(name);
+                        break;
+                    } else {
+                        out.println("NAME ALREADY EXISTS");
+                    }
+                }
+                out.println("NAME ACCEPTED");
+                writers.add(out);
+
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println(name + ": " + input);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (names != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                for (PrintWriter writer : writers) {
+                    writer.println(name + " has left the chat.");
+                }
+            }
+        }
+    }
 }
